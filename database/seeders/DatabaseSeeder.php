ff2ae0ce190f6d64d554f6b8f4e48cf44229ccc2
Marco Ledesma <marco.j.ledesma@gmail.com>
2024-07-17 10:21:57 -0400

    Revert ":construction:"
    This reverts commit f280e63cb8ebaf09806f59b4bef48f680a229440.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        if (User::count() === 0) {
-            User::factory()->create([
-                'name' => 'Test User',
-                'email' => 'test@example.com',
-            ]);
-        }
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
