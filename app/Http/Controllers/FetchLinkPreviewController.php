fc535cf51d0318960339d1b98b8274c658b8ee80
Marco Ledesma <marco.j.ledesma@gmail.com>
2024-08-02 23:19:31 -0400

    :construction: adds some docs

--- app/Http/Controllers/FetchLinkPreviewController.php
+++ app/Http/Controllers/FetchLinkPreviewController.php
@@ -9,11 +9,22 @@ use Illuminate\Support\Facades\Http;
 
 class FetchLinkPreviewController extends Controller
 {
+    //TODO: Although this works, its ugly, and needs to be refactored to smaller components
+    //Also, some websites will not return a valid response, such as those requiring a login.
+    //Therefore, this API needs to work with Saloon, an API plugin that lets us build integrations with other tools
+    //I imagine something like - if I paste in a URL and its detected that its from Github, then we can use the Github API to fetch the data
+    //The user should be prompted in the frontend to provide us with a personal access token (so we don't need to go through the OAuth flow).
+    //This token should be stored in the database, and used to make requests to the Github API.
+    //Likewise with JIRA
+
     public function __invoke(Request $request)
     {
         $url = $request->url;
-        if (Cache::has($url)) {
-            //return response()->json(Cache::get($url));
+        //the url may sometimes be too big to store as a key in the hash table
+        $cacheKey = md5($url);
+
+        if (Cache::has($cacheKey)) {
+            return response()->json(Cache::get($cacheKey));
         }
 
         $html = Http::get($url)->body();
@@ -47,10 +58,13 @@ class FetchLinkPreviewController extends Controller
                     $baseUrl = parse_url($url, PHP_URL_SCHEME) . '://' . parse_url($url, PHP_URL_HOST);
                     $image = $baseUrl . $faviconRelativePath;
                 }
+                else {
+                    $image = $faviconRelativePath;
+                }
             }
         }
 
-        Cache::put($url, [
+        Cache::put($cacheKey, [
             'title' => $title,
             'description' => $description,
             'image' => $image,
